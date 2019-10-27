17e858e1bda7f8abbd34a2cfe4348bc6a5a3928a
JOHN NELSON <JohnNelson4850@gmail.com>
2019-10-27 16:22:01 -0700

    route not defined

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-var foo = require("http");
+var http = require("http");
 var url = require("url");
 
 
 function start() {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");   
+    console.log("Request for " + pathname + " received.");
+    
+    route(pathname); 
+
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
   }
-  foo.createServer(onRequest).listen(8888)
+  http.createServer(onRequest).listen(8888)
 
   console.log('server is running');
 
