2fb81523223c6d85747a41dda95e1c6e56d431ec
JOHN NELSON <JohnNelson4850@gmail.com>
2019-10-27 16:40:17 -0700

    request handlers

---
+++ requestHandlers.js
@@ -0,0 +1,12 @@
+function start() {
+  console.log("Request handler 'start' was called.");
+  
+}
+
+function upload() {
+  console.log("request handler 'upload' was called");
+  
+}
+
+exports.start = start;
+exports.upload = upload;
