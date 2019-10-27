5ef95f89d1a5b7d03e129a33237fd16514c91326
JOHN NELSON <JohnNelson4850@gmail.com>
2019-10-27 15:57:13 -0700

    server as a module

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var http = require("http");
+function start() {
+  function onRequest(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
+  http.createServer(onRequest).listen(8888)
 
-function onRequest(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
+  console.log('server is running');
+
 }
 
-http.createServer(onRequest).listen(8888)
+exports.start = start;
