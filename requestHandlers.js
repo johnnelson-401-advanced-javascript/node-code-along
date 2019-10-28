60d929338f7997807a7444fa2ce32d803c325ccf
JOHN NELSON <JohnNelson4850@gmail.com>
2019-10-27 17:16:39 -0700

    exec

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,12 +1,15 @@
+var exec = requires("child_process").exec;
+
+
 function start() {
   console.log("Request handler 'start' was called.");
-
-  function sleep(milliSeconds) {
-    var startTime = new Date().getTime();
-    while (new Date().getTime() < startTime + milliSeconds);
-  }
-  sleep(10000);
-  return "hello start";
+    var content = "empty";
+    
+    exec("ls -lah", function (error, stdout, stderr) {
+      content = stdout
+    });
+    
+  return content;
 }
 
 function upload() {
