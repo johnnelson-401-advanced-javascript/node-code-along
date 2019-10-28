d33d5a83649e4894c1f988344c587c3b1bafd023
JOHN NELSON <JohnNelson4850@gmail.com>
2019-10-27 17:06:33 -0700

    missing return statement on router.js

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 function route(handle, pathname) {
   console.log("About to route a request for " + pathname);
-  if(typeof handle[pathname] === 'function') {
-    handle[pathname]();
+  if (typeof handle[pathname] === 'function') {
+    return handle[pathname]();
   } else {
     console.log('No request for handler found at ' + pathname);
     return "404 not found";
